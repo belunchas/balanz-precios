863b287e6200a9f4964b5ec9b0ada2c9a66917dd
belunchas <belugarci@gmail.com>
2026-04-09 10:19:27 -0300

    Actualizar precios 2026-04-09

---
+++ precios.js
@@ -0,0 +1,7 @@
+// precios.js - Generado por actualizar_precios.py
+// Ultima actualizacion: 2026-04-09 10:19
+// Tickers: 0 | Registros: 0
+
+var PRECIOS_HISTORICOS = {};
+
+var PRECIOS_META = {"ultima_actualizacion": "2026-04-09 10:19", "n_tickers": 0, "n_registros": 0, "desde": "2022-01-01", "errores": []};
